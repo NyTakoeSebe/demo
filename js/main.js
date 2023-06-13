cfbc9543a63cd67890878cf3bfff9c85f39b4f37
NyTakoeSebe <evd.matvey@mail.ru>
2023-06-13 14:38:36 +0700

    reviews

--- js/main.js
+++ js/main.js
@@ -15,3 +15,37 @@ closeMenuButton.addEventListener('click', () => {
     menu.classList.remove('open');
   }, 100);
 });
+
+// reviews slider
+const reviewsSlider = new Swiper('.reviews__slider', {
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 3,
+  spaceBetween: 20,
+  pagination: {
+    el: '.reviews__slider-pagination.swiper-pagination',
+    clickable: true,
+    renderBullet: function () {
+      return `<span class="reviews__slider-bullet swiper-pagination-bullet"></span>`;
+    },
+  },
+  breakpoints: {
+    970: {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    770: {
+      slidesPerView: 2.5,
+    },
+    620: {
+      slidesPerView: 2,
+    },
+    300: {
+      slidesPerView: 1,
+    },
+  },
+  navigation: {
+    nextEl: '.reviews__slider-next',
+    prevEl: '.reviews__slider-prev',
+  },
+});
