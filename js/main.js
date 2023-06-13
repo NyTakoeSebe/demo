639e643b23e4a9602878b46e9a6212e1a9be9b83
NyTakoeSebe <evd.matvey@mail.ru>
2023-06-13 21:51:57 +0700

    home page

--- js/main.js
+++ js/main.js
@@ -54,3 +54,44 @@ const reviewsSlider = new Swiper('.reviews__slider', {
 const phoneMask = IMask(document.getElementById('phone'), {
   mask: '+{7} (000) 000-00-00',
 });
+
+// scroll
+(function () {
+  const smoothScroll = function (targetEl, duration) {
+    const headerElHeight = document.querySelector('.header').clientHeight;
+    let target = document.querySelector(targetEl);
+    let targetPosition = target.getBoundingClientRect().top - headerElHeight;
+    let startPosition = window.scrollY;
+    let startTime = null;
+
+    const ease = function (t, b, c, d) {
+      t /= d / 2;
+      if (t < 1) return (c / 2) * t * t + b;
+      t--;
+      return (-c / 2) * (t * (t - 2) - 1) + b;
+    };
+
+    const animation = function (currentTime) {
+      if (startTime === null) startTime = currentTime;
+      const timeElapsed = currentTime - startTime;
+      const run = ease(timeElapsed, startPosition, targetPosition, duration);
+      window.scrollTo(0, run);
+      if (timeElapsed < duration) requestAnimationFrame(animation);
+    };
+    requestAnimationFrame(animation);
+  };
+
+  const scrollTo = function () {
+    const links = document.querySelectorAll('.js-scroll');
+    links.forEach((each) => {
+      each.addEventListener('click', function () {
+        const currentTarget = this.getAttribute('href');
+        if (menu.classList.contains('open')) {
+          menu.classList.remove('open');
+        }
+        smoothScroll(currentTarget, 1000);
+      });
+    });
+  };
+  scrollTo();
+})();
